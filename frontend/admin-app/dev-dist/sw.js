0fd98b090394625d832ddf2c815226bc9b919a55
hamnanoof <hamnanoof@gmail.com>
2026-06-15 17:08:25 +0530

    Fix order total mismatch between checkout, order details, and admin dashboard

--- frontend/admin-app/dev-dist/sw.js
+++ frontend/admin-app/dev-dist/sw.js
@@ -81,13 +81,13 @@ define(['./workbox-bbbdf4d3'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.69kd32qampc"
+    "revision": "0.9boh7lkud2o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
     allowlist: [/^\/$/]
   }));
-  workbox.registerRoute(/^https:\/\/api\.restaurantpwa\.com\/.*/i, new workbox.NetworkFirst({
+  workbox.registerRoute(/^https:\/\/api\.directdine\.com\/.*/i, new workbox.NetworkFirst({
     "cacheName": "api-cache",
     plugins: [new workbox.ExpirationPlugin({
       maxEntries: 50,
